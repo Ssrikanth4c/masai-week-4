32c9cd50d4366c4b53134e8cba119f3e49810fa3
srikanth <srikanthbinf@gmail.com>
2020-03-26 23:37:12 +0530

    project tic tac toe

--- week_4/script.js
+++ week_4/script.js
@@ -62,7 +62,7 @@ function addDetails(){
 
     var pEnterBtn=document.createElement('button')
     pEnterBtn.setAttribute('id', 'pEnterBtn')
-    pEnterBtn.textContent='Enter'
+    pEnterBtn.textContent='Tic Tac Toe'
     //apped name,input, slect sym, button to p1--div
     playerDiv.append(player1Name, p1Input,player2Name, p2Input,p1SelectName,p1Select,p2SelectName,p2Select,pEnterBtn)
 
@@ -116,11 +116,11 @@ function getPlayerDetails(){
     board.setAttribute('id', 'board')
     var leftSide=document.createElement('div')
     leftSide.setAttribute('id', 'left')
-    leftSide.innerHTML=player1+'<br>'+p1Sym
+    leftSide.innerHTML='<b>'+player1+'</b>'+'<br>'+p1Sym
     
     var rightSide=document.createElement('div')
     rightSide.setAttribute('id', 'right')
-    rightSide.innerHTML=player2+'<br>'+p2Sym
+    rightSide.innerHTML='<b>'+player2+'</b>'+'<br>'+p2Sym
     
     
     for(var i=1;i<=9;i++){
@@ -133,37 +133,111 @@ function getPlayerDetails(){
     GameBoard.append(leftSide,board,rightSide)
     document.body.append(GameBoard)
     
-    var count=0
+    var count=0, fillCell=0
     board.addEventListener('click', function(){
         if(count%2==0){
-            event.target.textContent=p1Sym
-            console.log(board.children[0].textContent)
+            if(event.target.textContent==''){
+                event.target.textContent=p1Sym
+                fillCell++
+            }
+            // console.log(event.target.textNode)
         }else{
-            event.target.textContent=p2Sym
+            if(event.target.textContent==''){
+                event.target.textContent=p2Sym
+                fillCell++
+            }
             console.log("odd")
         }
         count++
 
-        var p2=0,p1=0
+        //draw
+        if(fillCell==9){
+            alert('Tie')
+            fillCell=0
+            clearBoard(board)
+        }
+
+        var p2=0,p1=0,col1P1=0,col1P2=0,col2P1=0,col2P2=0,
+            col3P1=0,col3P2=0,r2P2=0,r2P1=0,
+            r3P2=0,r3P1=0,dia1P1=0,dia1P2=0,
+            dia2P1=0,dia2P2=0
+        //row1
         for(var i=0;i<3;i++){
             if(board.children[i].textContent==p2Sym){
-                // console.log(player2+'/t wins')
-                p2++
+               p2++
             }else if(board.children[i].textContent==p1Sym)
             {
                 p1++
             }
-            console.log(p2)
+            //row2
+            if(board.children[i+3].textContent==p2Sym){
+                r2P2++
+            }else if(board.children[i+3].textContent==p1Sym)
+            {
+                r2P1++
+            }
+            // row3
+            if(board.children[i+3*2].textContent==p2Sym){
+                r3P2++
+            }else if(board.children[i+3*2].textContent==p1Sym)
+            {
+                r3P1++
+            }
+            //col-1
+            if(board.children[i*3].textContent==p2Sym){
+               col1P2++
+            }else if(board.children[i*3].textContent==p1Sym)
+            {
+                col1P1++
+            }
+            //col2
+            if(board.children[i*3+1].textContent==p2Sym){
+                col2P2++
+            }else if(board.children[i*3+1].textContent==p1Sym)
+            {
+                col2P1++
+            }
+            // col3
+            if(board.children[i*3+2].textContent==p2Sym){
+                col3P2++
+            }else if(board.children[i*3+2].textContent==p1Sym)
+            {
+                col3P1++
+            }
+            // diagonal 0 4 8
+            if(board.children[i*4].textContent==p2Sym){
+                dia1P2++
+            }else if(board.children[i*4].textContent==p1Sym)
+            {
+                dia1P1++
+            }
+             // diagonal 2 4 6
+             if(board.children[i*2+2].textContent==p2Sym){
+                dia2P2++
+            }else if(board.children[i*2+2].textContent==p1Sym)
+            {
+                dia2P1++
+            }
+
+            
         }
         
-        if(p2==3){
+        
+
+        if(p2==3 || col1P2==3 || col2P2==3|| col3P2==3||r2P2==3||r3P2==3||dia1P2==3||dia2P2==3){
             alert(player2+'\tWins')
+            fillCell=0
            clearBoard(board)
+           p2=0,col1P2=0,col2P2=0,col3P2=0,r2P2=0,r3P2=0,dia1P2=0,dia2P2=0
         }
-        if(p1==3){
+        if(p1==3 || col1P1==3|| col2P1==3 ||col3P1==3||r2P1==3 ||r3P1==3||dia1P1==3||dia2P1==3 ){
             alert(player1+'\t Wins')
             clearBoard(board)
+            fillCell=0
+            p1=0,col1P1=0,col2P1=0,col3P1=0,r2P1=0,r3P1=0,dia1P1=0,dia2P1=0
         }
+        
+        console.log(item2.textContent)
     })
 }
 
